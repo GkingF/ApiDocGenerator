de7b0c4150c6a808d6aaf9d571afb0ddb9284562
fangsy <fangsy@infoepoch.com>
2022-08-18 00:27:02 +0800

    hutool

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,11 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+// https://mvnrepository.com/artifact/cn.hutool/hutool-all
+    implementation("cn.hutool:hutool-all:5.8.5")
+}
+
 // Set the JVM language level used to compile sources and generate files - Java 11 is required since 2020.3
 kotlin {
     jvmToolchain {
