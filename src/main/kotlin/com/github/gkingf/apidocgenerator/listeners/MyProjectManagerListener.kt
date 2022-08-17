7dd70969d6afa7705a65393cce3f55b759b8bab3
GitHub Action <action@github.com>
2022-08-17 14:15:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/gkingf/apidocgenerator/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gkingf/apidocgenerator/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gkingf.apidocgenerator.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.gkingf.apidocgenerator.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/gkingf/apidocgenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gkingf/apidocgenerator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gkingf.apidocgenerator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gkingf.apidocgenerator.MyBundle
 
 class MyProjectService(project: Project) {
 
